cd3f828af46d771025d3dfcd2159a52343e6c6ad
Julien Carayon <juliencarayon@MacBookJulien.local>
2024-03-15 15:41:44 +0100

    fixes merge issues

--- src/global/global.c
+++ src/global/global.c
@@ -1,111 +1,125 @@
 #include "global.h"
 #include "stm32f4xx_hal_uart.h"
 
+bool is_comptetion_started = false;
 
 const osMutexAttr_t uartMutex_attributes = {
     .name = "uartMutex",
-    osMutexPrioInherit,  // attr_bits
-    NULL,                // memory for control block   
-    0U                   // size for control block
+    osMutexPrioInherit, // attr_bits
+    NULL,               // memory for control block
+    0U                  // size for control block
 };
 
-void putsMutex(char *text){
-    if( uartMutex_M != NULL ){
-        osStatus_t aquire_status = osMutexAcquire( uartMutex_M, osWaitForever);
-        if( aquire_status != osOK)
+void putsMutex(char *text)
+{
+    if (uartMutex_M != NULL)
+    {
+        osStatus_t aquire_status = osMutexAcquire(uartMutex_M, osWaitForever);
+        if (aquire_status != osOK)
             // puts("osMutexAcquire failed :");
             // puts(osStatusToString(aquire_status));
             return;
-        puts(text); 
-        osStatus_t release_status = osMutexRelease( uartMutex_M );
-        if(release_status != osOK){
+        puts(text);
+        osStatus_t release_status = osMutexRelease(uartMutex_M);
+        if (release_status != osOK)
+        {
             // puts("osMutexrelease failed :");
             // puts(osStatusToString(release_status));
             return;
         }
-        
     }
 }
 
-char *getsMutex(char *text){
+char *getsMutex(char *text)
+{
     char *original_str = text;
-    if( uartMutex_M != NULL ){
-        osStatus_t aquire_status = osMutexAcquire( uartMutex_M, osWaitForever);
-        if( aquire_status != osOK)
-            return NULL;  // Retourne NULL en cas d'échec de l'acquisition du mutex
+    if (uartMutex_M != NULL)
+    {
+        osStatus_t aquire_status = osMutexAcquire(uartMutex_M, osWaitForever);
+        if (aquire_status != osOK)
+            return NULL; // Retourne NULL en cas d'échec de l'acquisition du mutex
 
         // Appeler la fonction gets avec le buffer alloué
-        gets(text);  
+        gets(text);
 
-        osStatus_t release_status = osMutexRelease( uartMutex_M );
-        if(release_status != osOK){
-            return NULL;  // Retourne NULL en cas d'échec de la libération du mutex
+        osStatus_t release_status = osMutexRelease(uartMutex_M);
+        if (release_status != osOK)
+        {
+            return NULL; // Retourne NULL en cas d'échec de la libération du mutex
         }
     }
     return original_str;
 }
 
-bool uart_data_available() {
+bool uart_data_available()
+{
     // Check if data is available to read from UART
     return (__HAL_UART_GET_FLAG(&huart2, UART_FLAG_RXNE) != RESET);
 }
 
-bool send_command(const char *command, char *response_buffer) {
-    if( uartMutex_M != NULL ){
-        osStatus_t aquire_status = osMutexAcquire( uartMutex_M, osWaitForever);
-        if( aquire_status != osOK)
-        return false;
-        
-        if (puts(command) < 0) {
+bool send_command(const char *command, char *response_buffer)
+{
+    if (uartMutex_M != NULL)
+    {
+        osStatus_t aquire_status = osMutexAcquire(uartMutex_M, osWaitForever);
+        if (aquire_status != osOK)
+            return false;
+
+        if (puts(command) < 0)
+        {
             return false; // Error sending command
         }
         // Receive response
         char *response = gets(response_buffer);
-        if (response == NULL) {
+        if (response == NULL)
+        {
             return false; // Error receiving response
         }
-        osStatus_t release_status = osMutexRelease( uartMutex_M );
-        if(release_status != osOK){
+        osStatus_t release_status = osMutexRelease(uartMutex_M);
+        if (release_status != osOK)
+        {
             // puts("osMutexrelease failed :");
             // puts(osStatusToString(release_status));
             return false;
         }
-        
     }
     return true; // Command sent successfully and response received
 }
 
-
-char* osStatusToString(osStatus_t status) {
-    switch (status) {
-        case osOK:
-            return "osOK";
-        case osError:
-            return "osError";
-        case osErrorTimeout:
-            return "osErrorTimeout";
-        case osErrorResource:
-            return "osErrorResource";
-        case osErrorParameter:
-            return "osErrorParameter";
-        case osErrorNoMemory:
-            return "osErrorNoMemory";
-        case osErrorISR:
-            return "osErrorISR";
-        case osStatusReserved:
-            return "osStatusReserved";
-        default:
-            return "Unknown Status";
+char *osStatusToString(osStatus_t status)
+{
+    switch (status)
+    {
+    case osOK:
+        return "osOK";
+    case osError:
+        return "osError";
+    case osErrorTimeout:
+        return "osErrorTimeout";
+    case osErrorResource:
+        return "osErrorResource";
+    case osErrorParameter:
+        return "osErrorParameter";
+    case osErrorNoMemory:
+        return "osErrorNoMemory";
+    case osErrorISR:
+        return "osErrorISR";
+    case osStatusReserved:
+        return "osStatusReserved";
+    default:
+        return "Unknown Status";
     }
 }
 
-char* longlong_to_string(long long num) {
+char *longlong_to_string(long long num)
+{
     static char buffer[32]; // Declaration of the output buffer
 
-    char* ptr = buffer; // Pointer to the beginning of the buffer
+    char *ptr = buffer; // Pointer to the beginning of the buffer
 
     // Check the sign of the number
-    if (num < 0) {
+    if (num < 0)
+    {
         *(ptr++) = '-';
         num = -num;
     }
@@ -113,13 +127,15 @@ char* longlong_to_string(long long num) {
     // Convert digits to characters, from right to left
     char temp[32];
     int digits = 0;
-    do {
+    do
+    {
         temp[digits++] = '0' + num % 10;
         num /= 10;
     } while (num != 0);
 
     // Copy the converted number characters into the output buffer, reversing the order
-    while (digits > 0) {
+    while (digits > 0)
+    {
         *(ptr++) = temp[--digits];
     }
 
@@ -128,4 +144,40 @@ char* longlong_to_string(long long num) {
 
     // Return the output buffer
     return buffer;
+}
+void generate_command(T_command_type command_type, int ship_id, int angle,
+                      int speed, char *command_buffer)
+{
+    switch (command_type)
+    {
+    case MOVE_CMD:
+        snprintf(command_buffer, 100, "MOVE %d %d %d\n", ship_id, angle, speed);
+        break;
+    case FIRE_CMD:
+        snprintf(command_buffer, 100, "FIRE %d %d\n", ship_id, angle);
+        break;
+    case RADAR_CMD:
+        snprintf(command_buffer, 100, "RADAR %d\n", ship_id);
+        break;
+    }
+}
+
+void set_ship_type(T_ship * ship)
+{
+    if (ship->id >= 1 && ship->id <= 5)
+    {
+        ship->ship_type = ATTACKERS_SHIP;
+    }
+    else if (ship->id > 5 && ship->id <= 7)
+    {
+        ship->ship_type = EXPLORER_SHIP;
+    }
+    else if (ship->id >= 8 && ship->id <= 9)
+    {
+        ship->ship_type = COLLECTOR_SHIP;
+    }
+    else
+    {
+        ship->ship_type = UNKNOWN_SHIP;
+    }
 }
--- src/main.c
+++ src/main.c
@@ -14,7 +14,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 osThreadId_t mainTaskHandle;
 void StartMainTask(void *argument);
 
@@ -30,148 +29,69 @@ int main(void)
     };
     mainTaskHandle = osThreadNew(StartMainTask, NULL, &mainTask_attributes);
 
-    attackerTaskHandles_1 = osThreadNew(StartAttackerTask_1, NULL, &attackersTask_attributes);
-    attackerTaskHandles_2 = osThreadNew(StartAttackerTask_2, NULL, &attackersTask_attributes);
-    attackerTaskHandles_3 = osThreadNew(StartAttackerTask_3, NULL, &attackersTask_attributes);
-    attackerTaskHandles_4 = osThreadNew(StartAttackerTask_4, NULL, &attackersTask_attributes);
-    attackerTaskHandles_5 = osThreadNew(StartAttackerTask_5, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_1 = osThreadNew(StartAttackerTask_1, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_2 = osThreadNew(StartAttackerTask_2, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_3 = osThreadNew(StartAttackerTask_3, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_4 = osThreadNew(StartAttackerTask_4, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_5 = osThreadNew(StartAttackerTask_5, NULL, &attackersTask_attributes);
 
-    exploratorTaskHandles_1 = osThreadNew(StartExploratorTask_1, NULL, &exploratorsTask_attributes);
-    exploratorTaskHandles_2 = osThreadNew(StartExploratorTask_2, NULL, &exploratorsTask_attributes);    
+    // exploratorTaskHandles_1 = osThreadNew(StartExploratorTask_1, NULL, &exploratorsTask_attributes);
+    // exploratorTaskHandles_2 = osThreadNew(StartExploratorTask_2, NULL, &exploratorsTask_attributes);
 
-    collectorTaskHandles_1 = osThreadNew(StartCollectorTask_1, NULL, &collectorsTask_attributes);
-    collectorTaskHandles_2 = osThreadNew(StartCollectorTask_2, NULL, &collectorsTask_attributes);
-    
+    // collectorTaskHandles_1 = osThreadNew(StartCollectorTask_1, NULL, &collectorsTask_attributes);
+    // collectorTaskHandles_2 = osThreadNew(StartCollectorTask_2, NULL, &collectorsTask_attributes);
+
     uartMutex_M = osMutexNew(&uartMutex_attributes);
     puts("BOOTED");
-
-
-typedef enum {
-  UNKNOWN_SHIP,
-  ATTACKERS_SHIP,
-  EXPLORER_SHIP,
-  COLLECTOR_SHIP
-} T_ship_type;
-
-typedef enum { MOVE_CMD, FIRE_CMD, RADAR_CMD } T_command_type;
-
-typedef struct {
-  int id;
-  int abscisse;
-  int ordinate;
-  int angle;
-  int speed;
-  bool status;
-  T_ship_type ship_type;
-
-} T_ship;
-
-char command_buffer[100];
-char start_command_server[50];
-bool is_comptetion_started = false;
-
-void set_ship_type(T_ship *ship);
-void generate_command(T_command_type command_type, int ship_id, int angle,
-                      int speed, char *command_buffer);
-
-osMutexId_t osMutexTest;
-
-int main(void) {
-  hardware_init();
-
-  osKernelInitialize();
-
-  const osThreadAttr_t defaultTask_attributes = {
-      .name = "defaultTask",
-      .priority = (osPriority_t)osPriorityNormal,
-      .stack_size = MINIMAL_STACK_SIZE,
-  };
-
-  defaultTaskHandle =
-      osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-
-  if (strstr(gets(start_command_server), "START") != NULL) {
-    if (osMutexRelease(osMutexTest) != osOK) {
-      return 0;
-    }
-
-    is_comptetion_started = true;
     osKernelStart();
-  } else {
-    is_comptetion_started = false;
-  }
-
-  while (1) {
-  }
-}
-
-void set_ship_type(T_ship *ship) {
-  if (ship->id >= 1 && ship->id <= 5) {
-    ship->ship_type = ATTACKERS_SHIP;
-  } else if (ship->id > 5 && ship->id <= 7) {
-    ship->ship_type = EXPLORER_SHIP;
-  } else if (ship->id >= 8 && ship->id <= 9) {
-    ship->ship_type = COLLECTOR_SHIP;
-  } else {
-    ship->ship_type = UNKNOWN_SHIP;
-  }
 }
 
 void StartMainTask(void *argument)
 {
     putsMutex("Main Thread Task started\n");
     while (1)
-    {   
-        putsMutex("Main Thread Task :\n");
+    {
+        // putsMutex("Main Thread Task :\n");
         char buffer[256]; // Utiliser un tableau pour stocker les données lues
-        if (uart_data_available()){
+        if (uart_data_available())
+        {
             getsMutex(buffer);
             putsMutex(buffer);
-            if(strstr(buffer,"START")!=NULL){
+            if (strstr(buffer, "START") != NULL)
+            {
                 putsMutex("GAME STARTED");
-                is_comptetion_started = true
+                is_comptetion_started = true;
             }
-            
-            int _angle = 90;
-            int _speed = 0;
-
- 
-            if (is_comptetion_started == true) {
-              if (_angle >= 359) {
-                _angle = 0;
-              }
-
-              if (_speed >= 2999) {
-                _speed = 0;
-              }
-
-              _angle += 10;
-              _speed += 10;
-
-              generate_command(MOVE_CMD, 4, _angle, _speed, command_buffer);
-              // generate_command(FIRE_CMD, 1, _angle, _speed, command_buffer);
-
-              puts(command_buffer);
-
-              osDelay(1);
-             } 
-             else {
-              /* do nothing */
-             }
         }
+        int _angle = 90;
+        int _speed = 0;
+        if (is_comptetion_started == true)
+        {
+            if (_angle >= 359)
+            {
+                _angle = 0;
+            }
+
+            if (_speed >= 2999)
+            {
+                _speed = 0;
+            }
+
+            _angle += 10;
+            _speed += 10;
+
+            generate_command(MOVE_CMD, 4, _angle, _speed, command_buffer);
+            // generate_command(FIRE_CMD, 1, _angle, _speed, command_buffer);
+
+            puts(command_buffer);
+
+            osDelay(1);
+        }
+        else
+        {
+            /* do nothing */
+        }
+
         osDelay(1);
     }
-void generate_command(T_command_type command_type, int ship_id, int angle,
-                      int speed, char *command_buffer) {
-  switch (command_type) {
-  case MOVE_CMD:
-    snprintf(command_buffer, 100, "MOVE %d %d %d\n", ship_id, angle, speed);
-    break;
-  case FIRE_CMD:
-    snprintf(command_buffer, 100, "FIRE %d %d\n", ship_id, angle);
-    break;
-  case RADAR_CMD:
-    snprintf(command_buffer, 100, "RADAR %d\n", ship_id);
-    break;
-  }
 }
