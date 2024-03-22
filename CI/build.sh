4468b72266bdc9051ba4df4f5b020882689a7dda
Jordan18122000 <jordclement@outlook.fr>
2024-03-22 16:37:59 +0100

    Ref #2 Modification of the script to apply MISRA on all files in src and addition of a test mode

--- CI/build.sh
+++ CI/build.sh
@@ -22,17 +22,68 @@ fi
 echo "___gcc-messages______"
 gcc -std=c99 -pedantic main.c -o /tmp/misra-c | zenity --progress --pulsate --auto-close --width=300 --title="Compilation in progress" --text="Compiling MISRA-C code..."
 
-# Vérifier si test-case.c existe
-if [ ! -f test-case.c ]; then
-    echo "Avertissement: le fichier test-case.c n'existe pas."
-else
-    # run test-case
-    echo "___test-case_________"
-    /tmp/misra-c < "$PROJECT_DIR/test-case.c"
+# Obtenir le répertoire source
+SRC_DIR="$(dirname "$PROJECT_DIR")/src"
+
+# Vérifier si le répertoire source existe
+if [ ! -d "$SRC_DIR" ]; then
+    echo "Erreur: le répertoire source n'existe pas."
+    exit 1
 fi
 
+# Se déplacer vers le répertoire src
+cd "$SRC_DIR" || exit 1
+echo "files pour repertoire SRC_DIR : $SRC_DIR"
+# Se déplacer vers le répertoire src
+# cd "$SRC_DIR" || exit 1
+
+# Afficher la liste des fichiers dans le répertoire src
+echo "Liste des fichiers dans le répertoire src :"
+ls -l
+
+# Liste des fichiers dans le répertoire src/
+#FILES="$SRC_DIR"/*
+FILES=$(find "$SRC_DIR" -type f)
+
+# Afficher les fichiers contenus dans $FILES
+echo "Contenu de la variable FILES :"
+echo "$FILES"
+
+# Si on est en mode test, tester uniquement le fichier main.c
+if [ "$TEST_MODE" == "true" ]; then
+    echo "Mode test activé. Testing seulement le fichier main.c."
+    /tmp/misra-c < main.c
+    exit 0
+fi
+
+# Si aucun fichier n'est présent dans le répertoire, afficher un message d'avertissement
+if [ -z "$FILES" ]; then
+    echo "Aucun fichier trouvé dans le répertoire src."
+    exit 1
+fi
+
+# Calculer le nombre total de fichiers
+TOTAL_FILES=$(ls -1 | wc -l)
+echo "TOTAL_FILES: $TOTAL_FILES"
+
+# Initialiser la progression
+CURRENT_PROGRESS=0
+INCREMENT=0
+
+
+for file in $FILES; do
+    #echo "___Running misra-c for file: $file"
+     # Increment progress
+    INCREMENT=$((100/TOTAL_FILES))
+    CURRENT_PROGRESS=$((CURRENT_PROGRESS+INCREMENT))
+    # Update progress bar
+    echo "$CURRENT_PROGRESS"
+
+    /tmp/misra-c < "$file"
+done
+
 # run self
-echo "___stdin_____________"
+echo "___Running misra-c for self"
 /tmp/misra-c
 
 echo "___end_______________"
