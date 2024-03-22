0fe9e9ad86814b7f042cf8c2629937227df3a2bc
Jordan18122000 <jordclement@outlook.fr>
2024-03-22 11:21:38 +0100

    Ref #2 Modifying the script to show a progress bar during compilation

--- CI/build.sh
+++ CI/build.sh
@@ -6,19 +6,17 @@ echo "___misra-c________________________________________________________"
 # (tmp folder is wiped at every reboot)
 echo "___gcc-messages______"
 #gcc -std=gnu99 main.c -o /tmp/misra-c
-gcc -std=c99 -pedantic main.c -o /tmp/misra-c
+gcc -std=c99 -pedantic main.c -o /tmp/misra-c | zenity --progress --pulsate --auto-close --width=300 --title="Compilation in progress" --text="Compiling MISRA-C code..."
 
 # run test-case
 echo "___test-case_________"
-/tmp/misra-c < ./test-case.c
+/tmp/misra-c < ./test-case.c | zenity --info --title="Test case output" --text-info --width=600 --height=400
 
 # run self
 echo "___stdin_____________"
-/tmp/misra-c
-
-
+/tmp/misra-c | zenity --info --title="Self output" --text-info --width=600 --height=400
 
 echo "___end_______________"
 
-
 rm /tmp/misra-c
+
