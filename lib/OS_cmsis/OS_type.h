8d322928087deb31fd99f7922507172f388ec3c0
Julien Carayon <juliencarayon@MacBookJulien.local>
2024-03-27 21:38:34 +0100

    trying to fix OS_types name instead of os_types

--- lib/OS_cmsis/OS_type.h
+++ lib/OS_cmsis/OS_type.h
@@ -13,7 +13,7 @@ typedef struct {
   uint16_t uint16_data_1;
   uint16_t uint16_data_2;
   uint16_t uint16_data_3;
-  uint8_t uint8_data;
+  uint8_t uint8_data_1;
   uint8_t uint8_data_2;
 } os_T_Memory_block;
 
