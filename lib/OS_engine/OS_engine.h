901d3f76a266674cab09248261ee724680986b3c
Julien Carayon <juliencarayon@MacBookJulien.local>
2024-03-28 20:22:09 +0100

    Trying memory pool

--- lib/OS_engine/OS_engine.h
+++ lib/OS_engine/OS_engine.h
@@ -9,7 +9,7 @@ extern bool is_comptetion_started;
 extern bool rx_command_received;
 extern char rx_command_buffer[RX_COMMAND_BUFFER_SIZE];
 
-os_memory_pool_id memory_pool_id;
+extern os_memory_pool_id memory_pool_id;
 
 void putsMutex(char *text);
 char *getsMutex(char *text);
@@ -21,6 +21,6 @@ void os_engine_init(void);
 os_mutex_id os_create_mutex(const os_mutex_attr mutex_attribute);
 void os_acquire_mutex(os_mutex_id mutex_id, uint32_t timeout);
 void os_release_mutex(os_mutex_id mutex_id);
-void init_memory_pool(void);
+os_memory_pool_id init_memory_pool(void);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -14,66 +14,83 @@
 osThreadId_t mainTaskHandle;
 void StartMainTask(void *argument);
 
-int main(void) {
-  hardware_init();
-  osKernelInitialize();
+os_memory_pool_id memory_pool_id = NULL;
 
-  os_engine_init();
+int main(void)
+{
+    hardware_init();
+    osKernelInitialize();
 
-  const osThreadAttr_t mainTask_attributes = {
-      .name = "mainTask",
-      .priority = (osPriority_t)osPriorityNormal1,
-      .stack_size = 2048,
-  };
-  mainTaskHandle = osThreadNew(StartMainTask, NULL, &mainTask_attributes);
+    os_engine_init();
 
-  attackerTaskHandles_1 =
-      osThreadNew(StartAttackerTask_1, NULL, &attackersTask_attributes);
-  attackerTaskHandles_2 =
-      osThreadNew(StartAttackerTask_2, NULL, &attackersTask_attributes);
-  attackerTaskHandles_3 =
-      osThreadNew(StartAttackerTask_3, NULL, &attackersTask_attributes);
-  attackerTaskHandles_4 =
-      osThreadNew(StartAttackerTask_4, NULL, &attackersTask_attributes);
-  attackerTaskHandles_5 =
-      osThreadNew(StartAttackerTask_5, NULL, &attackersTask_attributes);
+    const osThreadAttr_t mainTask_attributes = {
+        .name = "mainTask",
+        .priority = (osPriority_t)osPriorityNormal1,
+        .stack_size = 2048,
+    };
+    mainTaskHandle = osThreadNew(StartMainTask, NULL, &mainTask_attributes);
 
-  exploratorTaskHandles_1 =
-      osThreadNew(StartExploratorTask_1, NULL, &exploratorsTask_attributes);
-  exploratorTaskHandles_2 =
-      osThreadNew(StartExploratorTask_2, NULL, &exploratorsTask_attributes);
+    attackerTaskHandles_1 =
+        osThreadNew(StartAttackerTask_1, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_2 =
+    //     osThreadNew(StartAttackerTask_2, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_3 =
+    //     osThreadNew(StartAttackerTask_3, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_4 =
+    //     osThreadNew(StartAttackerTask_4, NULL, &attackersTask_attributes);
+    // attackerTaskHandles_5 =
+    //     osThreadNew(StartAttackerTask_5, NULL, &attackersTask_attributes);
 
-  collectorTaskHandles_1 =
-      osThreadNew(StartCollectorTask_1, NULL, &collectorsTask_attributes);
-  collectorTaskHandles_2 =
-      osThreadNew(StartCollectorTask_2, NULL, &collectorsTask_attributes);
+    // exploratorTaskHandles_1 =
+    //     osThreadNew(StartExploratorTask_1, NULL, &exploratorsTask_attributes);
+    // exploratorTaskHandles_2 =
+    //     osThreadNew(StartExploratorTask_2, NULL, &exploratorsTask_attributes);
 
-  osKernelStart();
+    // collectorTaskHandles_1 =
+    //     osThreadNew(StartCollectorTask_1, NULL, &collectorsTask_attributes);
+    // collectorTaskHandles_2 =
+    //     osThreadNew(StartCollectorTask_2, NULL, &collectorsTask_attributes);
+
+    osKernelStart();
 }
 
-void StartMainTask(void *argument) {
-  osStatus_t status;
-  os_T_Memory_block *memory_block;
-  os_memory_pool_id memory_pool_id = NULL;
+void StartMainTask(void *argument)
+{
+    osStatus_t status;
 
-  memory_block = (os_T_Memory_block *)osMemoryPoolAlloc(memory_pool_id,
-                                                        0U); // get Mem Block
-  if (memory_block != NULL) {        // Mem Block was available
-    memory_block->uint16_data_1 = 1; // do some work...
-    memory_block->uint16_data_2 = 2;
-    memory_block->uint16_data_3 = 3;
-    memory_block->uint8_data_1 = 4;
-    memory_block->uint8_data_2 = 5;
+    memory_pool_id = init_memory_pool();
+    putsMutex("memory_pool_id : ");
+    putsMutex(memory_pool_id);
+    putsMutex("\n");
+    os_T_Memory_block *memory_block = NULL;
+    memory_block = (os_T_Memory_block *)osMemoryPoolAlloc(memory_pool_id,
+                                                          osWaitForever); // get Mem Block
+    if (memory_block != NULL)
+    {                                      // Mem Block was available
+        memory_block->uint16_data_1 = 192; // do some work...
+        memory_block->uint16_data_2 = 174;
+        memory_block->uint16_data_3 = 23;
+        memory_block->uint8_data_1 = 244;
+        memory_block->uint8_data_2 = 53;
 
-    status = osMemoryPoolFree(memory_pool_id, memory_block); // free mem block
+        // status = osMemoryPoolFree(memory_pool_id, memory_block); // free mem block
 
-    if (status == osErrorNoMemory || status == osErrorParameter) {
-      while (1)
-        ;
+        // if (status == osErrorNoMemory || status == osErrorParameter)
+        // {
+        //     while (1)
+        //         ;
+        // }
     }
-  }
+    else
+    {
+        while (1)
+        {
+            puts("MEM POOL WRITE : memory_block is NULL\n");
+        }
+    }
 
-  while (1) {
-    osDelay(1);
-  }
+    while (1)
+    {
+        osDelay(1);
+    }
 }
