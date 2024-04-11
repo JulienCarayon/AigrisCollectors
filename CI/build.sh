7f701457c9fd75c7b64c814f0f667136ac64f2f8
Jordan18122000 <jordclement@outlook.fr>
2024-04-11 19:27:31 +0200

    Ref #2 Update script for check rules MISRA 2023

--- CI/build.sh
+++ CI/build.sh
@@ -3,8 +3,7 @@
 # Nom du fichier de sortie
 OUTPUT_DUMP=".dump"
 OUTPUT_CTU=".ctu-info"
-OUTPUT_FILE="output.txt"
-OUTPUT_PDF="output.pdf"
+OUTPUT_FILE="_MISRA.txt"
 
 # Fonction pour afficher la barre de progression
 progress_bar() {
@@ -26,7 +25,7 @@ TOTAL_FILES=$(ls -1 | wc -l)
 
 CURRENT_PROGRESS=0
 INCREMENT=0
-
+NEEDS_MODIFICATION=false
 # Boucler à travers chaque fichier .c trouvé
 for file in $files; do
     INCREMENT=$((100/TOTAL_FILES))
@@ -40,18 +39,28 @@ for file in $files; do
     cppcheck --dump "$file"
     python3.9 CI/misra.py "$file.dump" --rule-texts CI/misra_2023.txt 2>> "$OUTPUT_FILE"
     cat $OUTPUT_FILE
-    OUTPUT_PDF="${file}_output.pdf" 
-    #sed -i 's/\\n/\'$'\n''/g' "$OUTPUT_FILE" 
-    #pandoc -f markdown "$OUTPUT_FILE" -o "$OUTPUT_PDF" 2>&1 | tee pandoc_error.log
 
-    #pandoc -f txt "$OUTPUT_FILE" --variable geometry:"a3paper,landscape" -o "$OUTPUT_PDF" 2>&1 | tee pandoc_error.log
-
-    if [ ${PIPESTATUS[0]} -ne 0 ]; then
-        echo "Erreur lors de la conversion en PDF. Veuillez consulter le fichier pandoc_error.log pour plus de détails."
-        exit 1
-    else
-        #rm "$OUTPUT_FILE"
-        rm "$OUTPUT_DUMP"
-        rm "$OUTPUT_CTU"
+    if grep -q "Required" "$OUTPUT_FILE"; then
+        echo "Le fichier $file nécessite des modifications selon les règles MISRA."
+        NEEDS_MODIFICATION=true
     fi
+    rm $OUTPUT_DUMP
+    rm $OUTPUT_CTU
 done
+
+if [ "$NEEDS_MODIFICATION" == "true" ]; then
+    echo ""
+    echo "TEST KO :"
+    echo "Les fichiers suivants nécessitent des modifications selon les règles MISRA :"
+    for file in $files; do
+        if grep -q "Required" "${file}_output.txt"; then
+            echo "$file"
+        fi
+    done
+    exit 1
+else
+    echo ""
+    echo "TEST OK :"
+    echo "Aucun fichier ne nécessite de modifications selon les règles MISRA."
+    exit 0
+fi
